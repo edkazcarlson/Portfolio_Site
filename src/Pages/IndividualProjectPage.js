25c0791d527b36a30bf99b36b3e8c03be581b22e
Edward Carlson <kazuya@comcast.net>
2020-08-14 12:29:16 -0700

    first commit

---
+++ src/Pages/IndividualProjectPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class IndividualProjectPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default IndividualProjectPage
---
+++ src/Pages/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class MainPage extends Component {
+
+    
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default MainPage
